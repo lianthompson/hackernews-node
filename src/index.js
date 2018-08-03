0f135f588d7f9c3e845615b5708f4d2ab08796a4
Lian Thompson <lianthompson@Lians-MacBook-Pro-2.local>
2018-08-03 11:26:17 -0700

    create separate schema file

--- src/index.js
+++ src/index.js
@@ -21,40 +21,30 @@ let links = [{
 }]
 
 // 1
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-    creator: String!
-    characters: Int!
-}
-`
-
-// 2
+let idCount = links.length
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
     },
-    // Link: {
-    //     id: (root) => root.id,
-    //     description: (root) => root.description,
-    //     url: (root) => root.url,
-    //     creator: (root) => root.creator,
-    //     characters: (root) => root.characters
-    //   }
+    Mutation: {
+        // 2
+        post: (root, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
+    },
 }
 
 // 3
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: 'schema.graphql',
     resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
