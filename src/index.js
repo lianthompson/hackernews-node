75724571d922962d4025451993e5d439fbe67b6a
Lian Thompson <lianthompson@Lians-MacBook-Pro-2.local>
2018-08-08 13:19:38 -0700

    add subscription

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const { Prisma } = require('prisma-binding')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
 
 // 1
 
 const resolvers = {
     Query,
     Mutation,
-    AuthPayload
+    AuthPayload,
+    Subscription,
 }
 
 // 3
